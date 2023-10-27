b6326c528b6f5f1fc5452c50ad2ed210ff0066d2
Денис Скурат <den.skur2016@yandex.ru>
2023-10-27 20:29:13 +0300

    Add categories api

---
+++ src/Microservices/Categories/API/Categories.API/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Categories.API.Controllers
+{
+    [ApiController]
+    [Route("api/categories")]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll(
+            CancellationToken cancellationToken = default)
+        {
+            return Ok(new
+            {
+                id = Guid.NewGuid(),
+                name = "test 1"
+            });
+        }
+    }
+}
